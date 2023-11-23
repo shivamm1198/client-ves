b95b580dd0a855f3e4f84597e34d432672a0c7ab
Shivam Mishra <137258461+shivamm1198@users.noreply.github.com>
2023-11-23 18:35:09 +0530

    Update settings.py

--- first_project/settings.py
+++ first_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uhgk^m&uq37r=h-p50elymb9#$ix^zu!5v(v3$ne-)ct+$1rj1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['first_project.onrender.com']
 
