e87b5dbea8cb1552ff6481531af43aebd097d923
Shivam Mishra <137258461+shivamm1198@users.noreply.github.com>
2023-11-23 18:46:46 +0530

    Update settings.py

--- first_project/settings.py
+++ first_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-uhgk^m&uq37r=h-p50elymb9#$ix^zu!5v(v3$ne-)ct+$1rj1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['first_project.onrender.com']
+ALLOWED_HOSTS = ['first_project.onrender.com', 'ves-website-iif0.onrender.com']
 
 
 # Application definition
