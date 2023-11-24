682df2685a01918c59d2972765c185f62123c030
Shivam Mishra <137258461+shivamm1198@users.noreply.github.com>
2023-11-24 17:22:01 +0530

    Update urls.py

--- first_app/urls.py
+++ first_app/urls.py
@@ -23,6 +23,9 @@ urlpatterns = [
     path('profile/edit/', views.edit_profile, name='edit_profile'),
 ]
 
+#    if settings.DEBUG:
+#       urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
 
